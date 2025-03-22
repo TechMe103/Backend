29d1a7b7d88e153baab361ab1d6cc67f2783ce94
TechMe103 <salunkhesanika610@gmail.com>
2025-03-21 23:38:10 -0700

    Backend

---
+++ math.js
@@ -0,0 +1,21 @@
+// const sum=(a, b) => a+b;
+//  or module.exports.sum=(a , b)=> a+b;
+//  or exports.mul=(a , b) => a*b;
+// const mul=(a , b) => a*b;
+// const g= 9.8;
+
+
+// // module.exports = 123;
+
+// let obj={
+//     sum : sum, 
+//     mul : mul, 
+//     g: g 
+// };
+// module.export= obj;
+
+
+//for importing
+export const sum=(a, b) => a+b;
+export const mul=(a , b) => a*b;
+export const g= 9.8;
