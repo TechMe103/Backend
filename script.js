d3550cba7862b59baef029a6b7b3a695f83af071
sanika Salunkhe <salunkhesanika610@gmail.com>
2025-03-22 21:10:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,28 @@
-// let n=5;
-// for(let i=0; i<=n; i++){
-//     console.log("Hello" , i);
-// }
-// console.log("Bye");
+let n=5;
+for(let i=0; i<=n; i++){
+    console.log("Hello" , i);
+}
+console.log("Bye");
 
-// console.log(process.argv);
+console.log(process.argv);
 
-// let args = process.argv;
+let args = process.argv;
 
-// for(let i=2; i< args.length; i++){
-//     console.log("Hello " , args[i]);
-// }
+for(let i=2; i< args.length; i++){
+    console.log("Hello " , args[i]);
+}
 
-//o/p
-// PS C:\Users\LENOVO\OneDrive\Desktop\Backend> node script.js sanika aditya sakshi atharva
-// Hello  sanika
-// Hello  aditya
-// Hello  sakshi
-// Hello  atharva
+  
+const  math= require("./math");
+console.log(math);
 
-
-// const  math= require("./math");
-// console.log(math);
-
-// console.log(math.sum(2 , 2));
-// console.log(math.g);
+console.log(math.sum(2 , 2));
+console.log(math.g);
 
 
 //requiring all info from fruits directory to script.js file
-// const info = require("./fruits");
-// console.log(info[0]);  //apple
-
-
-//op
-// PS C:\Users\LENOVO\OneDrive\Desktop\Backend> cd fruits
-// PS C:\Users\LENOVO\OneDrive\Desktop\Backend\fruits> cd..
-// PS C:\Users\LENOVO\OneDrive\Desktop\Backend> node script.js
-// [
-//   { name: 'Apple', color: 'red' },
-//   { name: 'Bananaa', color: 'yellow' },
-//   { name: 'Mango', color: 'orange' }
+const info = require("./fruits");
+console.log(info[0]);  //apple
 
 
 //for importing 
@@ -47,5 +30,5 @@ import {sum , g} from "./math.js";
 console.log(sum(1 , 2));
 
 //hv to generate random-words packages
-//import { generate } from "random-words";
-//console.log(generate());
+import { generate } from "random-words";
+console.log(generate());
